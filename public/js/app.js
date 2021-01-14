26d1b20d236b3cb342b65824fadfdc6113d412f0
Steven Mercer <stevenmercer9913@gmail.com>
2021-01-14 15:32:24 -0800

    app.js is finished

--- public/js/app.js
+++ public/js/app.js
@@ -23,6 +23,7 @@ class App extends React.Component {
       this.setState({
         books: response.data,
         name: '',
+        author: '',
         fiction: '',
         image: '',
         description:'',
@@ -30,6 +31,34 @@ class App extends React.Component {
       })
     )
   }
+
+  deleteBook = event => {
+  axios
+    .delete('/books/' + event.target.value)
+    .then(response => {
+      this.setState({
+        books: response.data
+      })
+    })
+}
+
+updateBook = event => {
+  event.preventDefault()
+  const id = event.target.id
+  axios
+   .put('/books/' + id, this.state)
+   .then(response => {
+      this.setState({
+        books: response.data,
+        name: '',
+        author: '',
+        fiction: '',
+        image: '',
+        description:'',
+        isCheckedOut: false,
+      })
+    })
+}
 
   componentDidMount = () => {
     axios
@@ -67,7 +96,78 @@ class App extends React.Component {
           <input type="submit" value="Create Book" />
         </form>
         <h2>These are the Books in the Library:</h2>
-        
+        <ul>
+          {this.state.books.map((book) => {
+            return (
+              <li key={book.id}>
+                {book.name} <br/>
+                {book.author} <br/>
+                {book.fiction} <br/>
+                {book.description} <br/>
+                <details>
+                  <summary>Edit this Book</summary>
+                    <form id={book._id} onSubmit={this.updateBook}>
+                    <label htmlFor="name">Name</label>
+                    <br />
+                    <input
+                      type="text"
+                      id="name"
+                      onChange={this.handleChange}
+                    />
+                    <br />
+                    <label htmlFor="author">Author</label>
+                    <br />
+                    <input
+                      type="text"
+                      id="author"
+                      onChange={this.handleChange}
+                    />
+                    <br />
+                    <label htmlFor="fiction">Fiction Type</label>
+                    <br />
+                    <input
+                      type="text"
+                      id="fiction"
+                      onChange={this.handleChange}
+                    />
+                    <br />
+                    <label htmlFor="image">Image</label>
+                    <br />
+                    <input
+                      type="text"
+                      id="image"
+                      onChange={this.handleChange}
+                    />
+                    <label htmlFor="description">Description</label>
+                    <br />
+                    <input
+                      type="text"
+                      id="description"
+                      onChange={this.handleChange}
+                    />
+                    <br />
+                    <input
+                      type="hidden"
+                      id="isCheckedOut"
+                      onChange={this.handleChange}
+                      value={false}
+                    />
+                    <br />
+                    <br />
+                    <input type="submit" value="Update Book" />
+                  </form>
+                </details>
+                <img src={book.image} alt={book.name} />
+                <button value={book._id} onClick={this.deleteBook}>
+                  DELETE
+                </button>
+                <button >
+                  Check Out This Book
+                </button>
+              </li>
+            )}
+          })
+        </ul>
       </div>
     )
   }

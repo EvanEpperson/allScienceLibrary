1acb9ba03c9fd05dc03b9eab7cb4841e8c4abf5f
Evan Epperson <evanepperson@pop-os.localdomain>
2021-01-14 14:33:01 -0600

    finished all routes and made post with postman

--- controllers/books_controller.js
+++ controllers/books_controller.js
@@ -2,12 +2,42 @@ const express = require('express');
 const books = express.Router()
 const Books = require('../models/books.js')
 // books leads to /books
+
+// main get route to actually see all of the books 
 books.get('/', (req, res) => {
     Books.find({}, (err, foundBooks) => {
         res.json(foundBooks)
     } )
 })
 
+//  edit route for editing books
+books.put('/:id', (req, res) => {
+    Books.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true},
+        (err, updatedBooks) => {
+            if(err){
+                res.send(err)
+            }else{
+                Books.find({}, (err, foundBooks) => {
+                    res.json(foundBooks)
+                })
+            }
+        }
+    )
+})
+
+// delete route for deleting books 
+books.delete('/:id', (req, res) => {
+    Books.findByIdAndRemove(req.params.id, (err, deletedBook) => {
+        Books.find({}, (err, foundBooks) => {
+            res.json(foundBooks)
+        })
+    })
+})
+
+// post route for creating books
 books.post('/', (req, res ) => {
     Books.create(req.body, (err, createdBooks) => {
         Books.find({}, (err, createdBooks) => {
@@ -15,12 +45,5 @@ books.post('/', (req, res ) => {
         })
     })
 })
-
-
-
-
-
-
-
-
+// exporting to whatever needs it 
 module.exports = books

ea159cff45413e0026f210801987ffaec92c60ea
Steven Mercer <stevenmercer9913@gmail.com>
2021-01-14 12:35:00 -0800

    create form complete

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,38 @@ class App extends React.Component {
     description:'',
     isCheckedOut: false
   }
+
+
+  render = () => {
+    return (
+      <div>
+        <h2>Add a Book to the Library</h2>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor="name">Name</label>
+          <input type="text" id="name" onChange={this.handleChange} value={this.state.name}/>
+          <br />
+          <label htmlFor="author">Author</label>
+          <input type="text" id="author" onChange={this.handleChange} value={this.state.author}/>
+          <br />
+          <label htmlFor="fiction">fiction Type</label>
+          <input type="text" id="fiction" onChange={this.handleChange} value={this.state.fiction}/>
+          <br />
+          <label htmlFor="image">Image</label>
+          <input type="text" id="image" onChange={this.handleChange} value={this.state.image}/>
+          <br />
+          <label htmlFor="description">Description</label>
+          <input type="text" id="description" onChange={this.handleChange} value={this.state.description}/>
+          <br />
+          <label htmlFor="isCheckedOut">Name</label>
+          <input type="hidden" id="isCheckedOut" onChange={this.handleChange} value=false/>
+          <br />
+          <input type="submit" value="Create Book" />
+        </form>
+      </div>
+    )
+  }
 }
+
 
 
 ReactDOM.render(

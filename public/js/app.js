24d951d1785a82bbe623306d6c4a05cff060cb81
Steven Mercer <stevenmercer9913@gmail.com>
2021-01-18 08:56:01 -0800

    more css for create form

--- public/js/app.js
+++ public/js/app.js
@@ -19,9 +19,7 @@ class App extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault()
-    axios
-    .post('/books', this.state)
-    .then((response) =>
+    axios.post('/books', this.state).then((response) =>
       this.setState({
         books: response.data,
         name: '',
@@ -87,48 +85,51 @@ class App extends React.Component {
   render = () => {
     return (
       <div className="main">
-        <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+        <div className="d-grid gap-2 d-md-flex justify-content-md-end dropstart">
           <button className="btn btn-outline-info create-button " type="button" data-bs-toggle="collapse" data-bs-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
           Add a New Book
           </button>
           <div className="collapse" id="collapseExample">
-            <div className="card card-body create-form-card" >
+            <div className="card card-body create-form-card create-form" >
               <form onSubmit={this.handleSubmit}>
                 <div className="form-floating mb-3">
                   <label htmlFor="name">Name</label>
-                  <input type="text" id="name" className="form-control" onChange={this.handleChange} value={this.state.name} required/>
-                  <br />
+                  <input type="text" id="name" className="input-box form-control" onChange={this.handleChange} value={this.state.name} required/>
                 </div>
+                <br />
                 <div className="form-floating mb-3">
                   <label htmlFor="author">Author</label>
-                  <input type="text" id="author" onChange={this.handleChange} value={this.state.author} required/>
-                  <br />
+                  <input type="text" id="author" className="form-control input-box" onChange={this.handleChange} value={this.state.author} required/>
                 </div>
+                <br />
                 <div className="form-floating">
-                  <select className="form-select" id="fiction" aria-label="Floating label select " onChange={this.handleChange} value={this.state.fiction} required>
+                  <select className="form-select input-box" id="fiction" aria-label="Floating label select " onChange={this.handleChange} value={this.state.fiction} required>
                     <option selected></option>
                     <option value="Fiction">Fiction</option>
                     <option value="Non-Fiction">Non-Fiction</option>
                   </select>
                   <label htmlFor="fiction">Fiction Type</label>
-                  {/*<input type="text" id="fiction" onChange={this.handleChange} value={this.state.fiction} required/>*/}
                 </div>
                 <br />
-                <label htmlFor="image">Image</label>
-                <input type="text" id="image" onChange={this.handleChange} value={this.state.image}/>
+                <div className="form-floating mb-3">
+                  <label htmlFor="image">Image</label>
+                  <input type="text" id="image" className="form-control input-box" onChange={this.handleChange} value={this.state.image}/>
+                </div>
                 <br />
-                <label htmlFor="description">Description</label>
-                <textarea id="description" onChange={this.handleChange} value={this.state.description}>
-                </textarea>
+                <div className="form-floating mb-3">
+                  <label htmlFor="description">Description</label>
+                  <textarea id="description" className="form-control input-box" onChange={this.handleChange} value={this.state.description}>
+                  </textarea>
+                </div>
                 <br />
                 <input type="hidden" id="isCheckedOut" onChange={this.handleChange} value={false}/>
                 <br />
-                <input type="submit" value="Create Book" />
+                <input className="btn btn-outline-info create-button" type="submit" value="Create Book" />
               </form>
             </div>
           </div>
         </div>
-        <h2>These are the Books in the Library:</h2>
+        <h2 className="these">These are the Books in the Library:</h2>
         <Book name={this.state.name} author={this.state.author} fiction={this.state.fiction} image={this.state.image} description={this.state.description} books={this.state.books}></Book>
       </div>
     )
